397b58210257cb90a149e3c73e590da59cad57b4
sysysysyb <baeksy6067@gmail.com>
2026-02-24 22:55:18 +0900

    feat: 공통 카드 컴포넌트 추가 (#8)
    - children 속성으로 자식 요소 추가 가능
    - className, props 속성으로 추가 스타일 및 속성 지정 가능
    - 자식 요소 사이즈만큼 width, height를 가짐

---
+++ developer-test-template/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card({ className, children, ...props }) {
+  return (
+    <div
+      className={`size-fit rounded-3xl bg-white p-8 shadow-[0_25px_50px_-12px_rgba(0,0,0,0.25)]`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Card;
