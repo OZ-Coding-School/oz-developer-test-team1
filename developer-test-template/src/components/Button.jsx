b897903d4fd15c598073f39f39609d4e456197a3
김우진 <kedw158@gmail.com>
2026-02-25 18:26:01 +0900

    docs: 버튼 컴포넌트 최종 수정 (#1)

--- developer-test-template/src/components/Button.jsx
+++ developer-test-template/src/components/Button.jsx
@@ -9,18 +9,18 @@ const Button = ({ label, size, variant, onClick }) => {
   const getVariantClass = () => {
     switch (variant) {
       case 'primary':
-        return 'bg-primary text-white shadow-[0_4px_15px_0_rgba(0,0,0,0.12)]'; // 메인 핑크 (시작하기, 결과 공유하기)
+        return 'bg-primary text-[#FFFFFF] shadow-[0_10px_15px_0_rgba(0,0,0,0.12)] rounded-full'; // 메인 핑크 (시작하기, 결과 공유하기)
       case 'secondary':
-        return 'bg-background text-primary shadow-sm'; // 연한 핑크
+        return 'bg-background text-[#EF6D8A] rounded-[16px]'; // 연한 핑크
       default:
-        return 'bg-background text-text-body shadow-sm'; // 질문 선택지용
+        return 'bg-background text-[#364153] shadow-[0_1px_3px_0_rgba(0,0,0,0.20)] rounded-[16px]'; // 질문 선택지용
     }
   };
 
   return (
     <button
       onClick={onClick}
-      className={` ${widthClass} ${getVariantClass()} flex h-14 cursor-pointer items-center justify-center rounded-full text-lg font-bold transition-all active:scale-95`}
+      className={` ${widthClass} ${getVariantClass()} flex h-14 cursor-pointer items-center justify-center text-lg font-bold transition-all active:scale-95`}
     >
       {/* 버튼 텍스트 */}
       <span>{label}</span>
--- developer-test-template/src/pages/HomePage.jsx
+++ developer-test-template/src/pages/HomePage.jsx
@@ -4,13 +4,27 @@ import Button from '../components/Button';
 export default function HomePage() {
   return (
     /* 배경색과 중앙 정렬 레이아웃 */
-    <div className="bg-background flex min-h-screen flex-col items-center justify-center gap-6">
-      <div className="text-text-heading text-2xl font-bold">
-        나는 어떤 개발자일까??
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-white p-10">
+      <h1 className="text-text-heading text-2xl font-bold">
+        나는 어떤 개발자일까 ?
+      </h1>
+
+      {/* Primary 버튼 */}
+      <div className="flex flex-col items-center gap-4">
+        <Button label="시작하기" size="small" variant="primary" />
+        <Button label="결과 공유하기" size="medium" variant="primary" />
       </div>
 
-      {/* 버튼 추가 */}
-      <Button label="시작하기" size="small" variant="primary" />
+      {/* Secondary 버튼 */}
+      <div className="flex flex-col items-center">
+        <Button label="다시 테스트하기" size="medium" variant="secondary" />
+      </div>
+
+      {/* Option 버튼 */}
+      <div className="flex flex-col gap-3">
+        <Button label="사용자가 볼 화면부터 디자인한다" size="medium" />
+        <Button label="데이터베이스 구조를 먼저 설계한다" size="medium" />
+      </div>
     </div>
   );
 }
