98e0e08363cb66ab15082c7d7c398c323f7e4e73
김우진 <kedw158@gmail.com>
2026-02-25 14:28:34 +0900

    fix: 조교님 피드백 반영 및 코드 수정

--- developer-test-template/src/components/Button.jsx
+++ developer-test-template/src/components/Button.jsx
@@ -8,15 +8,15 @@ const Button = ({ label, size, variant, onClick }) => {
   // 디자인 시스템 색상 설정
   const colorClass =
     variant === 'primary'
-      ? 'bg-[#EF6D8A] text-white' // 메인 핑크 (시작하기, 결과 공유하기)
+      ? 'bg-primary text-white' // 메인 핑크 (시작하기, 결과 공유하기)
       : variant === 'secondary'
-        ? 'bg-[#FDF2F3] text-[#EF6D8A]' // 연한 핑크
-        : 'bg-[#F8F0F2] text-[#1E2939]'; // 질문 선택지용
+        ? 'bg-secondary text-primary' // 연한 핑크
+        : 'bg-sub text-gray-700'; // 질문 선택지용
 
   return (
     <button
       onClick={onClick}
-      className={` ${widthClass} ${colorClass} flex h-[56px] cursor-pointer items-center justify-center rounded-full text-[18px] font-bold transition-all active:scale-95`}
+      className={` ${widthClass} ${colorClass} flex h-14 cursor-pointer items-center justify-center rounded-full text-lg font-bold transition-all active:scale-95`}
     >
       {/* 버튼 텍스트 */}
       <span>{label}</span>
