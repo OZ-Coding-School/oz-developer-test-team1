5a058c015788ac3fff31cf2ebe491f7f53ca551e
jeonjihoo11 <wjstndus41@gmail.com>
2026-02-25 15:59:32 +0900

    style: 뱃지 레이아웃수정 (#2)

--- developer-test-template/src/components/Badge.jsx
+++ developer-test-template/src/components/Badge.jsx
@@ -4,7 +4,7 @@ function Badge({ tags }) {
       {tags.map((tag) => (
         <div
           key={tag}
-          className="bg-background flex items-center rounded-full px-[12px] py-[6px]"
+          className="bg-background flex items-center rounded-full px-3 py-[6px]"
         >
           <span className="font-inter text-primary text-sm leading-5 font-medium tracking-[-0.15px]">
             #{tag}
