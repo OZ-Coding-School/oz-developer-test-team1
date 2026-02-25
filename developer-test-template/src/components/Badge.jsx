587450ca0295ffce90e0b9cd498ab3202c9e10c2
jeonjihoo11 <wjstndus41@gmail.com>
2026-02-25 15:03:25 +0900

    style: 레이아웃수정 (#2)

--- developer-test-template/src/components/Badge.jsx
+++ developer-test-template/src/components/Badge.jsx
@@ -2,14 +2,14 @@ function Badge({ tags }) {
   return (
     <div className="flex flex-wrap gap-2">
       {tags.map((tag) => (
-        <span
+        <div
           key={tag}
-          className="bg-background flex h-[32px] w-[70.67px] items-center rounded-full"
+          className="bg-background flex items-center rounded-full px-[12px] py-[6px]"
         >
-          <span className="font-inter text-primary h-[20px] w-[47px] pl-[12px] text-[14px] leading-[20px] font-medium tracking-[-0.15px]">
+          <span className="font-inter text-primary text-sm leading-5 font-medium tracking-[-0.15px]">
             #{tag}
           </span>
-        </span>
+        </div>
       ))}
     </div>
   );
