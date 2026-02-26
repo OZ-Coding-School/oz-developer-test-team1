8ecfa2817af112a50c885753ce4e860a0f03dbac
jeonjihoo11 <wjstndus41@gmail.com>
2026-02-26 13:35:39 +0900

    feat: add icons (#13)

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -8,7 +8,15 @@ function App() {
     <>
       <HomePage />
       <Badge tags={['react', 'css']} />
-      <Icon name={'start'} />
+      {/*뱃지영역*/}
+      {/*아이콘 데모영역 캡쳐용 아이콘이 다화면에 보이게끔 임시조치 해놓앗습니다 화면에 잠깐 보이게끔만 해놓은거라
+      필요하면 없애셔도 되요 */}
+      <div className="flex items-center gap-6">
+        <Icon name={'refresh'} />
+        <Icon name={'start'} />
+        <Icon name={'share'} />
+      </div>
+      {/*아이콘 영역*/}
     </>
   );
 }
