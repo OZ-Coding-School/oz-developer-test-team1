410dc2760985325001504014080b0382ee673fa2
jeonjihoo11 <wjstndus41@gmail.com>
2026-02-27 13:33:51 +0900

    feat: 동적 라우팅 구현 (#19)

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -3,15 +3,16 @@ import Badge from './components/Badge';
 
 import HomePage from './pages/HomePage';
 import { Routes, Route } from 'react-router';
+import QuestionPage from './pages/QuestionPage';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={null} />{' '}
+        <Route path="/" element={<HomePage />} />{' '}
         {/*연결하도록 설정해놓음, 바꾸셔도 되요 */}
-        <Route path="/question" element={null} />
-        <Route path="/result" element={null} />
+        <Route path="/question" element={<QuestionPage />} />
+        <Route path="/result" element={null} /> {/*결과페이지로 연결되는곳 */}
       </Routes>
 
       <HomePage />
