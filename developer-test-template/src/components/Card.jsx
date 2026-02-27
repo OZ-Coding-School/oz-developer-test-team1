5822138382f7615fbf1ac0c9ab0c158f3048c9ab
sysysysyb <baeksy6067@gmail.com>
2026-02-27 14:36:52 +0900

    refactor: Card 컴포넌트 className 수정 (#23)
    - className에 외부 스타일 추가 지정을 위해 className props 적용

--- developer-test-template/src/components/Card.jsx
+++ developer-test-template/src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card({ className, children, ...props }) {
   return (
     <div
-      className={`size-fit rounded-3xl bg-white p-8 shadow-[0_25px_50px_-12px_rgba(0,0,0,0.25)]`}
+      className={`size-fit rounded-3xl bg-white p-8 shadow-[0_25px_50px_-12px_rgba(0,0,0,0.25)] ${className}`}
       {...props}
     >
       {children}
