f230beb9bf89fc2957ea24457b522ec8d56d454c
sysysysyb <baeksy6067@gmail.com>
2026-03-03 13:47:10 +0900

    refactor: 주석 삭제 (#32)

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -9,8 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />{' '}
-        {/*연결하도록 설정해놓음, 바꾸셔도 되요 */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/question" element={<QuestionPage />} />
         <Route path="/result" element={null} /> {/*결과페이지로 연결되는곳 */}
       </Routes>
