747ada746bed97ee95af854dc888c428b4f5b2a7
sysysysyb <baeksy6067@gmail.com>
2026-03-03 13:49:17 +0900

    refactor: 불필요한 코드 삭제 및 import 순서 정리 (#32)

--- developer-test-template/src/App.jsx
+++ developer-test-template/src/App.jsx
@@ -1,22 +1,15 @@
 import './App.css';
-import Badge from './components/Badge';
-
-import HomePage from './pages/HomePage';
 import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
 import QuestionPage from './pages/QuestionPage';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/question" element={<QuestionPage />} />
-        <Route path="/result" element={null} /> {/*결과페이지로 연결되는곳 */}
-      </Routes>
-
-      <HomePage />
-      <Badge tags={['react', 'css']} />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/question/:id" element={<QuestionPage />} />
+      <Route path="/result" element={null} /> {/*결과페이지로 연결되는곳 */}
+    </Routes>
   );
 }
 
