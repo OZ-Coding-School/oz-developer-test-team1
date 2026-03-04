7bf2f04bba1f708e2ec4e3854929dbaa58b38f22
김우진 <kedw158@gmail.com>
2026-03-04 15:49:39 +0900

    feat: 사용자 선택 답변 저장 로직 구현 (#41)

--- developer-test-template/src/pages/QuestionPage.jsx
+++ developer-test-template/src/pages/QuestionPage.jsx
@@ -5,11 +5,14 @@ import ProgressBar from '../components/ProgressBar';
 import Button from '../components/Button';
 import CharacterIcon from '../components/CharacterIcon';
 import { getQuestions } from '../api/services/questions';
+import { useTestResult } from '../store/useTestResult';
 
 export default function QuestionPage() {
   const [questions, setQuestions] = useState([]);
   const [currentStep, setCurrentStep] = useState(1);
   const navigate = useNavigate();
+
+  const addAnswer = useTestResult((state) => state.addAnswer);
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -28,7 +31,9 @@ export default function QuestionPage() {
   const currentData = questions[currentStep - 1];
   const totalSteps = questions.length;
 
-  const handleNext = () => {
+  const handleNext = (type) => {
+    addAnswer(type);
+
     if (currentStep < totalSteps) {
       setCurrentStep((prev) => prev + 1);
     } else {
@@ -59,7 +64,7 @@ export default function QuestionPage() {
               label={option.text}
               variant="default"
               className="text-base"
-              onClick={handleNext}
+              onClick={() => handleNext(option.type)}
             />
           ))}
         </div>
